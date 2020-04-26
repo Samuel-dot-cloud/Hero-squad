56338de1222ccf83eca64c6f5db0e21cc3ae4239
Samuel-dot-cloud <successam336@gmail.com>
2020-04-26 23:44:26 +0300

    Definition of properties in hero class

---
+++ src/main/java/models/Hero.java
@@ -0,0 +1,15 @@
+package models;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Hero{
+    private String name;
+    private int age;
+    private String power;
+    private String weakness;
+    private int id;
+    private int squadId;
+    private static List<Hero> instances = new ArrayList<Hero>();
+
+
+}
