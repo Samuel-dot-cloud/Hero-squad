ee6ab255d8de86da98a60a6e2f63aff2f8cd9578
Samuel-dot-cloud <successam336@gmail.com>
2020-04-27 11:44:13 +0300

    Initialization of tests

--- src/test/java/models/HeroTest.java
+++ src/test/java/models/HeroTest.java
@@ -18,11 +18,55 @@ public class HeroTest {
 
     @Test
     public void heroCreatesInstanceOfHero_true() throws Exception {
-        Hero hero=setUpNewHero();
-        assertEquals(true,hero instanceof Hero);
+        Hero hero = setUpNewHero();
+        assertTrue(String.valueOf(true), true);
     }
 
-    private Hero setUpNewHero() {
-        return new Hero("Superman", 24, "strength", "kryptonite");
+    @Test
+    public void returnAllInstancesOfHeroes() throws Exception {
+        Hero hero = setUpNewHero();
+        Hero otherHero = new Hero("Aqualad", 20, "control the sea ", "Can't breathe normal air", 2);
+        assertEquals(2, Hero.getHeroes().size());
     }
+
+    @Test
+    public void allInstancesAreContainedInHero() {
+        Hero hero = setUpNewHero();
+        Hero otherHero = new Hero("Aqualad", 20, "control the sea ", "Can't breathe normal air", 2);
+        assertTrue(Hero.getHeroes().contains(hero));
+        assertTrue(Hero.getHeroes().contains(otherHero));
+    }
+
+    @Test
+    public void findById() throws Exception {
+        Hero hero = setUpNewHero();
+        Hero otherHero = new Hero("Aqualad", 20, "control the sea ", "Can't breathe normal air", 2);
+        Hero foundHero = Hero.findHero(1);
+        assertEquals(hero, foundHero);
+    }
+
+    @Test
+    public void deleteAllHeroes() {
+        Hero hero = setUpNewHero();
+        Hero otherHero = new Hero("Aqualad", 20, "control the sea ", "Can't breathe normal air", 2);
+        Hero.clearAll();
+        assertEquals(0, Hero.getHeroes().size());
+
+    }
+
+    @Test
+    public void deleteOneHero() {
+        Hero hero=setUpNewHero();
+        Hero otherHero = new Hero("Aqualad", 20, "control the sea ", "Can't breathe normal air", 2);
+        hero.getId();
+        assertEquals(1,Hero.getHeroes().size());
+        assertEquals(Hero.getHeroes().get(0).getId(),2);
+
+
+    }
+
+        private Hero setUpNewHero () {
+            return new Hero("Superman", 24, "strength", "kryptonite", 1);
+        }
+
 }
